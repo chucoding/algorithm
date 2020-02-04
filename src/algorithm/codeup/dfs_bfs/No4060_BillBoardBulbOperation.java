8ec188c111314f9236fdf0cf7a8693cc8049e574
chucoding <chucoding@gmail.com>
2020-02-04 17:08:55 +0900

    재귀로 풀이 성공

--- src/algorithm/codeup/dfs_bfs/No4060_BillBoardBulbOperation.java
+++ src/algorithm/codeup/dfs_bfs/No4060_BillBoardBulbOperation.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class No4060_BillBoardBulbOperation {
 
 	static int[][] visited = null;
+	static int[][] billBoard = null;
 	
 	public static void main(String[] args) {
 		
@@ -22,7 +23,7 @@ public class No4060_BillBoardBulbOperation {
 			if(m>=2 && m<=100 && n>=2 && n<=100) break;
 		}
 
-		int[][] billBoard = new int[m][n]; 
+		billBoard = new int[m][n]; 
 		visited = new int[m][n];
 		
 		for(int i=0; i<m; i++) {
@@ -40,7 +41,7 @@ public class No4060_BillBoardBulbOperation {
 					if(billBoard[i][j] == 1) turnon++;
 					else turndown++;
 					
-					dfs(i, j);
+					dfs(i, j, i, j);
 					
 				} else {
 					continue;
@@ -48,22 +49,20 @@ public class No4060_BillBoardBulbOperation {
 			}
 		}
 		
-		System.out.println("킨 횟수 : "+turnon);
-		System.out.println("끈 횟수 : "+turndown);
+		System.out.print(turndown+" "+turnon);
 	}
-	
-	public static void dfs(int x, int y) {
+		
+	public static void dfs(int x, int y, int i, int j) {
 
-		if(x==0 || y==0 || x==visited.length-1 || y==visited[0].length-1) return;
+		if(x<0 || y<0 || x>visited.length-1 || y>visited[0].length-1) return;
 		if(visited[x][y] == 1) return; //방문했다면 종료
+		if(billBoard[i][j] != billBoard[x][y]) return;
 		
 		visited[x][y] = 1;
 		
-		dfs(x-1, y); //위쪽 탐색
-		dfs(x, y-1); //왼쪽 탐색
-		dfs(x+1, y); //오른쪽 탐색
-		dfs(x, y+1); //아래쪽 탐색
-		
+		dfs(x-1, y, i, j); //위쪽 탐색
+		dfs(x, y-1, i, j); //왼쪽 탐색
+		dfs(x+1, y, i, j); //오른쪽 탐색
+		dfs(x, y+1, i, j); //아래쪽 탐색
 	}
 }
-
