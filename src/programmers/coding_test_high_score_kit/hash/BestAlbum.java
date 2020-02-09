a04facae3ea11638c427457712163928c4be7875
chucoding <chucoding@gmail.com>
2020-02-09 16:34:17 +0900

    스택 탑 문제 풀이 추가

---
+++ src/programmers/coding_test_high_score_kit/hash/BestAlbum.java
@@ -0,0 +1,25 @@
+package programmers.coding_test_high_score_kit.hash;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//난이도 3짜리!!
+public class BestAlbum {
+
+	public static int[] solution(String[] genres, int[] plays) {
+        int[] answer = {};
+        
+        Map map = new HashMap<>();
+//        map.put(key, value)
+        
+        return answer;
+    }
+	
+	public static void main(String[] args) {
+		
+		String[] genres = {"classic", "pop", "classic", "classic", "pop"};
+		int[] plays = {500, 600, 150, 800, 2500};
+		
+		int[] arr = solution(genres, plays);
+	}
+}
---
+++ src/programmers/coding_test_high_score_kit/stack_queue/Tower.java
@@ -0,0 +1,41 @@
+package programmers.coding_test_high_score_kit.stack_queue;
+
+import java.util.Stack;
+
+public class Tower {
+
+	public static int[] solution(int[] heights) {
+		
+		int[] answer = new int[heights.length];
+        Stack<Integer> stack = new Stack<>();
+
+        for(int height : heights) {
+        	stack.push(height);
+        }
+        
+        while(!stack.isEmpty()) {
+
+        	int height = stack.pop();
+
+        	for(int i=stack.size(); i>=0; i--) {
+        		if(height < heights[i] ) {
+        			answer[stack.size()] = i+1;
+        			break;
+        		}
+        	}
+        }
+        
+        return answer;
+    }
+	
+	public static void main(String[] args) {
+		
+		int[] heights = {6,9,5,7,4};
+		int[] res = solution(heights);
+		
+		for(int i : res) {
+			System.out.print(i+" ");
+		}
+	}
+}
+
