f8d9358fba4b2382e89160c8ac20122b8f7fcd35
chucoding <chucoding@gmail.com>
2020-02-10 23:07:06 +0900

    그리디 체육복 문제풀이 - 실패(이거 왜 안되는지 모르겠음)

---
+++ src/programmers/coding_test_high_score_kit/stack_queue/greedy/GymSuit.java
@@ -0,0 +1,60 @@
+package programmers.coding_test_high_score_kit.stack_queue.greedy;
+
+public class GymSuit {
+
+	public static int solution(int n, int[] lost, int[] reserve) {
+
+		int answer = 0;
+        int[] student = new int[n];
+        
+        for(int i=0; i<n; i++) {
+        	student[i] = 1;
+        }
+        
+        for(int i=0; i<reserve.length; i++) {
+        	student[reserve[i]-1] = 2;
+        }
+        
+        for(int i=0; i<lost.length; i++) {
+        	student[lost[i]-1] = 0;
+        }
+
+        for(int i=0; i<n; i++) {
+
+        	if(student[i] == 0) {
+        		if(i == 0 && student[i+1] == 2) {
+        			student[i]++;
+            		student[i+1]--;
+        		} else if(i == n-1 && student[i-1] == 2) {
+        			student[i]++;
+            		student[i-1]--;
+        		} else {
+        			if(i != 0 && student[i-1] == 2) {
+        				student[i]++;
+        				student[i-1]--;
+        			} else if(i != n-1 && student[i+1] == 2) {
+        				student[i]++;
+        				student[i-1]--;
+        			} 
+        		}
+        		
+        	}
+        }
+        
+        for(int i=0; i<n; i++) {
+        	if(student[i] > 0) answer++;
+        }
+        
+        System.out.println(answer);
+        
+        return answer;
+    }
+	
+	public static void main(String[] args) {
+		
+		solution(5, new int[]{2, 4}, new int[]{1, 3, 5});
+		solution(5, new int[]{2, 4}, new int[]{3});
+		solution(10, new int[]{1,3,4,5}, new int[]{2,9});
+		
+	}
+}
