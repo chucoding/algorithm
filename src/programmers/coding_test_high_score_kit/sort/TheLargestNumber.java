2ac168a2f9656fcb4f1e5435d9816ff5d1a59198
chucoding <chucoding@gmail.com>
2020-02-10 16:59:44 +0900

    String으로 더하는 방법이 있었음!

--- src/programmers/coding_test_high_score_kit/sort/TheLargestNumber.java
+++ src/programmers/coding_test_high_score_kit/sort/TheLargestNumber.java
@@ -1,32 +1,37 @@
 package programmers.coding_test_high_score_kit.sort;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+import java.util.Comparator;
 
 public class TheLargestNumber {
 
 	public static String solution(int[] numbers) {
 
-		String answer = "";
-		int[] _numbers = new int[numbers.length];
-		
-        for(int i=0; i<numbers.length; i++) {
-        	
-        	int num = numbers[i];
-        	
-        	while(true) {
-        		if(num < 10) {
-        			_numbers[i] = num;
-        			break;
-        		}
-        		num /= 10;
-        	}
-        }
-        
-        Arrays.sort(_numbers); //생각해보니 같은수면?? 으으..  // 넣으면서 정렬해야됨
-        
-        return answer;
+		 String answer = "";
+	        
+		 //int 배열을 String 배열로 변환
+		 String[] arr = new String[numbers.length];
+		 for (int i = 0; i < numbers.length; i++) {
+			 arr[i] = (String.valueOf(numbers[i]));
+		 }
+
+		 //배열 정렬, 정렬 규칙으로는 2개를 더하여 더 큰 쪽이 우선순위가 있도록 정렬
+		 Arrays.sort(arr, new Comparator<String>() {
+			 @Override
+			 public int compare(String s1, String s2) {
+				 return (s2+s1).compareTo(s1+s2);
+			 }
+		 });
+
+		 //0000 처럼 0으로만 구성되어있으면 0 return
+		 if (arr[0].equals("0")) return "0";
+
+		 //그 외의 경우 순차적으로 연결하여 answer return
+		 for (int i = 0; i < arr.length; i++) {
+			 answer+=arr[i];
+		 }
+		 
+		 return answer;
     }
 	
 	public static void main(String[] args) {
