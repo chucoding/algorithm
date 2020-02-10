2ab279c6550b140a34f3611b4396d03a10f80492
chucoding <chucoding@gmail.com>
2020-02-10 18:20:20 +0900

    break 부분이랑 역순 바꿔주니 됨.. 문제가 뭔진 모르겠음

--- src/programmers/coding_test_high_score_kit/bp/MockTest.java
+++ src/programmers/coding_test_high_score_kit/bp/MockTest.java
@@ -2,19 +2,18 @@ package programmers.coding_test_high_score_kit.bp;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class MockTest {
 
 	public static int[] solution(int[] answers) {
         int[] answer = {};
-        List list = new ArrayList();
+        List<Integer> list = new ArrayList<Integer>();
         
         int[] a = {1,2,3,4,5};
         int[] b = {2,1,2,3,2,4,2,5};
         int[] c = {3,3,1,1,2,2,4,4,5,5};
-        Integer[] cntArr = {0, 0, 0};
+        int[] cntArr = new int[3];
         
         for(int i=0; i<answers.length; i++) {
         	if(answers[i] == a[i%5]) cntArr[0]++;
@@ -22,26 +21,29 @@ public class MockTest {
         	if(answers[i] == c[i%10]) cntArr[2]++;
         }
         
-        Arrays.sort(cntArr, Collections.reverseOrder());
+        int winner = cntArr[0];
         for(int i=0; i<cntArr.length; i++) {
-        	
-        	if(i == 0 || cntArr[i] == cntArr[i-1]) list.add(i+1);
-        	else break;
+        	if(winner < cntArr[i]) {
+        		winner = cntArr[i];
+        	}
+        }
+        
+        for(int i=0; i<cntArr.length; i++) {
+        	if(winner == cntArr[i]) list.add(i);
         }
         
         answer = new int[list.size()];
         for(int i=0; i<answer.length; i++) {
-        	answer[i] = (int) list.get(i);
+        	answer[i] = list.get(i)+1;
         }
         
         Arrays.sort(answer);
-        
         return answer;
     }
 	
 	public static void main(String[] args) {
 		
-		int[] answer = {1,2,3,4,5};
+		int[] answer = {1,2,3,4,5,4,3,4,2,3,4,2,2,2};
 		solution(answer);
 	}
 }
