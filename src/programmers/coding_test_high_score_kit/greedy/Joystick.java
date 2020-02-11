a15d1ff00c93613e260c306ec0f4d19cea6be370
chucoding <chucoding@gmail.com>
2020-02-11 13:25:49 +0900

    조이스틱 푸는중

--- src/programmers/coding_test_high_score_kit/greedy/Joystick.java
+++ src/programmers/coding_test_high_score_kit/greedy/Joystick.java
@@ -2,12 +2,14 @@ package programmers.coding_test_high_score_kit.greedy;
 
 public class Joystick {
 
-	public int solution(String name) {
+	public static int solution(String name) {
 		int answer = 0;
 		return answer;
 	}
 	
 	public static void main(String[] args) {
-		
+		int sol = solution("JEROEN");
 	}
+	
+	
 }
