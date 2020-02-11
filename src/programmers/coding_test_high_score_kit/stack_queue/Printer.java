fc8fca3c6ada8abc68b938b382c92e5bcf38fa7a
chucoding <chucoding@gmail.com>
2020-02-12 04:31:10 +0900

    짜증,,

--- src/programmers/coding_test_high_score_kit/stack_queue/Printer.java
+++ src/programmers/coding_test_high_score_kit/stack_queue/Printer.java
@@ -22,6 +22,7 @@ public class Printer {
 	}
 	
 	public static void main(String[] args) {
+		System.out.println(solution(new int[] {1,6,3,1,2,5,6,7,2,3}, 3));
 		System.out.println(solution(new int[] {2,1,3,2}, 2));
 		System.out.println(solution(new int[] {1,1,9,1,1,1}, 0));
 	}
