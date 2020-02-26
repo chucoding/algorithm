fa53be539d0c5ec30fe34373a93e4753840e8913
chucoding <chucoding@gmail.com>
2020-02-26 16:45:05 +0900

    라면공장 풀이?? 전에날짜 구하는거 모르겠음

--- src/programmers/coding_test_high_score_kit/heap/RamenFactory2.java
+++ src/programmers/coding_test_high_score_kit/heap/RamenFactory2.java
@@ -1,10 +1,6 @@
 package programmers.coding_test_high_score_kit.heap;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
 import java.util.Queue;
 
 public class RamenFactory2 {
@@ -12,22 +8,40 @@ public class RamenFactory2 {
 	public static int solution(int stock, int[] dates, int[] supplies, int k) {
 		int answer = 0;
 		
-		Map map = new HashMap();
-		Queue q = new LinkedList();
+		Queue<Integer> dq = new LinkedList();
+		Queue<Integer> sq = new LinkedList();
 		
 		for(int i=0; i<dates.length; i++) {
-			map.put(dates[i],supplies[i]);
-			q.add(map);
+			dq.add(dates[i]);
+			sq.add(supplies[i]);
 		}
-		
-		int i=0;
-		while(k > stock + supplies[i]) {
-
-			if(stock >= dates[i])
+	
+		while(k > stock) {
+	
+			int max_supplies = 0;
+			int today = 0;
+			
+			//현 재고가 버틸 수 있는 날짜 전부 구해서 그 날짜중에 최고에 해당하는 값을 구함 
+			while(true)
 			{
-				List list = new ArrayList();
-				list.add(supplies[i]);
+				if(dq.isEmpty() || sq.isEmpty()) break;
+				
+				//가능한 날짜를 넘어버림
+				if(stock < dq.peek()) break;
+				
+				today = dq.poll();
+				int tmp = sq.poll();
+				
+				//가능한 날짜중 공급량 최대를 구함
+				if(max_supplies < tmp) {
+					max_supplies = tmp;
+				}
 			}
+			
+			stock += max_supplies;
+			//stock -= today - ? ;
+			
+			answer++;
 			
 		}
 		
