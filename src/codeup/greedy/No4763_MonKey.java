57434097bc757f93ff644aa5e25d0b44a5cb29c6
chucoding <chucoding@gmail.com>
2020-03-17 07:10:36 +0900

    코드업 원숭이 문제 인풋값 출력 성공

---
+++ src/codeup/greedy/No4763_MonKey.java
@@ -0,0 +1,37 @@
+package codeup.greedy;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class No4763_MonKey {
+	public static void main(String[] args) {
+		File file = new File("G:/test/INPUT.TXT");
+
+		FileInputStream fis = null;
+		StringBuffer sb = new StringBuffer(); 
+		
+		int in = -1;
+		int len = 0;
+		
+		try {
+			fis = new  FileInputStream(file);
+
+			while((in = fis.read()) != -1) sb.append((char)in);
+		
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				fis.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		System.out.println(sb);
+	}
+}
