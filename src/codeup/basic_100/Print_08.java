8f908e55ac3c1cace063b4df1f1dff45c209bf75
chucoding <chucoding@gmail.com>
2020-03-20 19:19:07 +0900

    추가

--- src/codeup/basic_100/Print_08.java
+++ src/codeup/basic_100/Print_08.java
@@ -5,6 +5,6 @@ public class Print_08 {
 	public static void main(String[] args) {
 		System.out.printf("\u250C\u252C\u2510\n");
 		System.out.printf("\u251C\u253C\u2524\n");
-		System.out.printf("\u2514\u2534\u2518\n");
+		System.out.printf("\u2514\u2534\u2518");
 	}
 }
