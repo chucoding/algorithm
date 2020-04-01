89c75f083993499f10e98abafcf88f3eb4243bbc
chucoding <chucoding@gmail.com>
2020-04-01 22:10:25 +0900

    네트워크 다른방법 풀이 추가

--- src/programmers/coding_test_high_score_kit/dfs_bfs/Network.java
+++ src/programmers/coding_test_high_score_kit/dfs_bfs/Network.java
@@ -59,9 +59,37 @@ public class Network {
         return answer;
     }
     
+
+    //다른방법(chk 배열이용한 dfs - 유니온파인드에 비해 속도 10배 빠름!)
+    public static int solution2(int n, int[][] computers) {
+    	 int answer = 0;
+         boolean[] chk = new boolean[n];
+         for(int i = 0; i < n; i++) {
+             if(!chk[i]) {
+                 dfs(computers, chk, i);
+                 answer++;
+             }
+         }
+         return answer;
+    }
+    
+    public static void dfs(int[][] computers, boolean[] chk, int start) {
+        chk[start] = true;
+        for(int i = 0; i < computers.length; i++) {
+            if(computers[start][i] == 1 && !chk[i]) {
+                dfs(computers, chk, i);
+            }
+        }
+    }
+    
     public static void main(String[] args) {
 //		solution(3, new int[][]{{1,1,0},{1,1,0},{0,0,1}});
 //		solution(4, new int[][]{{1,1,0,0},{1,1,0,0},{0,0,1,0},{0,0,0,1}});
-		solution(4, new int[][]{{1,0,0,1},{0,1,1,1},{0,1,1,0},{1,1,0,1}});
-	}
+
+    	long a = System.nanoTime();
+    	solution2(4, new int[][]{{1,0,0,1},{0,1,1,1},{0,1,1,0},{1,1,0,1}});
+    	long b = System.nanoTime();
+    	
+    	System.out.println(b-a);
+    }
 }
