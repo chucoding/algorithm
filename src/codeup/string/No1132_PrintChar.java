288086cde6e9ace3bd75d062998b0bf14563510e
chucoding <chucoding@gmail.com>
2020-04-02 13:22:02 +0900

    코드업 문자 출력 문제 풀이

---
+++ src/codeup/string/No1132_PrintChar.java
@@ -0,0 +1,11 @@
+package codeup.string;
+
+import java.util.Scanner;
+
+public class No1132_PrintChar {
+
+	private static Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		System.out.println(sc.next().charAt(0));
+	}
+}
