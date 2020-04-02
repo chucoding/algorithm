3440742e4c85fb8b48e9c86a5a97a29860aa181f
chucoding <chucoding@gmail.com>
2020-04-02 17:14:09 +0900

    거꾸로 출력 문제 완료

---
+++ src/codeup/stack/No1402_PrintingBackwards3.java
@@ -0,0 +1,28 @@
+package codeup.stack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class No1402_PrintingBackwards3 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=0; i<n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		System.out.println(n);
+
+		for(int i=n-1; i>=0; i--) {
+			System.out.print(arr[i]+" ");
+		}
+	}
+}
