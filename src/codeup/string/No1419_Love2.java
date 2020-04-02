c55c5553fea6fbbc58a7f36a1011d8c594079e6e
chucoding <chucoding@gmail.com>
2020-04-02 14:02:28 +0900

    자바 love2문제 풀이

---
+++ src/codeup/string/No1419_Love2.java
@@ -0,0 +1,26 @@
+package codeup.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class No1419_Love2 {
+
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			String s = br.readLine();
+
+			int lineCnt = 0;
+			int fromIndex = -1;
+			while((fromIndex = s.indexOf("love", fromIndex+1)) >=0) {
+				lineCnt++;
+			}
+			
+			System.out.println(lineCnt);
+		
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
