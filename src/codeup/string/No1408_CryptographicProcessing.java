e5820de165404669af3aaee3491e0ceba9124387
chucoding <chucoding@gmail.com>
2020-04-03 20:12:13 +0900

    암호처리 문제 최적화

--- src/codeup/string/No1408_CryptographicProcessing.java
+++ src/codeup/string/No1408_CryptographicProcessing.java
@@ -7,23 +7,22 @@ import java.io.InputStreamReader;
 public class No1408_CryptographicProcessing {
 
 	public static void main(String[] args) throws IOException {
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
 		char[] str = br.readLine().toCharArray();
-		
-		long x = System.nanoTime();
-		
-		for(int i=0; i<str.length; i++) {
-			System.out.print((char) (str[i] + 2));
-		}
-		
-		System.out.println();
+		char[] a = new char[str.length];
+		char[] b = new char[str.length];
 		
 		for(int i=0; i<str.length; i++) {
-			System.out.print((char) ((str[i] * 7) % 80 + 48));
+			a[i] = (char) (str[i] + 2);
+			b[i] = (char) ((str[i] * 7) % 80 + 48);
 		}
 		
-		long y = System.nanoTime();
-		System.out.println();
-		System.out.println(y-x);
+		String.valueOf(a);
+		String.valueOf(b);
+		
+		System.out.println(a);
+		System.out.println(b);
 	}
 }
