32b047104eabc54ae76ed7216d739bf34f3140fa
chucoding <chucoding@gmail.com>
2020-04-03 17:59:04 +0900

    Welcome문제 풀이

---
+++ src/codeup/string/No1734_Welcome.java
@@ -0,0 +1,14 @@
+package codeup.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class No1734_Welcome {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String id = br.readLine();
+		System.out.println("welcome! "+id);
+	}
+}
