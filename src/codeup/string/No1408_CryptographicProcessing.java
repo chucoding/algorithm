efdce6f14b2e5862154d4e1610f7799d7783f070
chucoding <chucoding@gmail.com>
2020-04-03 19:58:26 +0900

    암호처리 문제 풀이

---
+++ src/codeup/string/No1408_CryptographicProcessing.java
@@ -0,0 +1,30 @@
+package codeup.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class No1408_CryptographicProcessing {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		char[] str = br.readLine().toCharArray();
+		char[] a = new char[str.length];
+		char[] b = new char[str.length];
+		
+		for(int i=0; i<str.length; i++) {
+			a[i] = (char) (str[i] + 2);
+			b[i] = (char) ((str[i] * 7) % 80 + 48);
+		}
+		
+		long x = System.nanoTime();
+		String.valueOf(a);
+		String.valueOf(b);
+		
+		System.out.println(a);
+		System.out.println(b);
+		long y = System.nanoTime();
+
+		System.out.println(y-x);
+	}
+}
