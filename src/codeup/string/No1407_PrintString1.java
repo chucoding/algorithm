b4eefb8004994ecf25a0db11a17231a090a229b3
chucoding <chucoding@gmail.com>
2020-04-03 18:23:36 +0900

    패턴매처랑 속도비교.. 별로 차이 없음! 짧은거 쓰자

--- src/codeup/string/No1407_PrintString1.java
+++ src/codeup/string/No1407_PrintString1.java
@@ -3,15 +3,17 @@ package codeup.string;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.regex.Pattern;
 
 public class No1407_PrintString1 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder(100);
 		
-		sb.append(br.readLine().replaceAll("\\s",""));
-		
-		System.out.println(sb);
+		long a = System.nanoTime();
+		System.out.println(Pattern.compile("\\s").matcher(br.readLine()).replaceAll(""));
+//		System.out.println(br.readLine().replaceAll("\\s",""));
+		long b = System.nanoTime();
+		System.out.println(b-a);
 	}
 }
