fe8c688f9545379bf98977b2a2a5a82e48bf8b72
chucoding <chucoding@gmail.com>
2020-04-06 18:39:22 +0900

    배스트앨범 풀이 실패

--- src/programmers/coding_test_high_score_kit/hash/BestAlbum.java
+++ src/programmers/coding_test_high_score_kit/hash/BestAlbum.java
@@ -1,8 +1,11 @@
 package programmers.coding_test_high_score_kit.hash;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -12,7 +15,32 @@ public class BestAlbum {
 
 	public static int[] solution(String[] genres, int[] plays) {
         int[] answer = {};
-        Map<String, Map<Integer, Integer>> map = new HashMap();
+        
+        Map<String,Integer> genresMap = new HashMap<String, Integer>();
+        
+        //장르별 많이 팔린대로 묶기
+        for(int i=0; i<genres.length; i++) {
+        	genresMap.put(genres[i], genresMap.getOrDefault(genres[i], 0)+plays[i]);
+        }
+ 
+        //장르 정렬
+        List<Map.Entry<String, Integer>> sortList = new LinkedList<>(genresMap.entrySet());
+        Collections.sort(sortList, new Comparator<Map.Entry<String,Integer>>(){
+			@Override
+			public int compare(Entry<String, Integer> o1, Entry<String, Integer> o2) {
+				int comparision = (o1.getValue() - o2.getValue()) * -1;
+    			return comparision;
+			}
+        });
+        
+        // 순서유지를 위해 LinkedHashMap을 사용
+        Map<String, Integer> sortedMap = new LinkedHashMap<>(); 
+        for(Iterator<Map.Entry<String, Integer>> iter = sortList.iterator(); iter.hasNext();){
+            Map.Entry<String, Integer> entry = iter.next();
+            sortedMap.put(entry.getKey(), entry.getValue());
+        }
+        
+        Map<String, Map<Integer, Integer>> map = new HashMap<String, Map<Integer, Integer>>();
         
         for(int i=0; i<genres.length; i++) {
         	if(map.get(genres[i]) != null) {
@@ -24,36 +52,26 @@ public class BestAlbum {
         	}
         }
         
+        answer = new int[map.size()*2];
+        int i = 0;
+        
         for(String genre : map.keySet()) {
         	Map<Integer,Integer> playsMap = map.get(genre);
-        	
-        	int sum = 0;
-        	for(Integer playIndex : playsMap.keySet()) {
-        		sum += map.get(genre).get(playIndex);
-        	}
-        	
         	List<Map.Entry<Integer, Integer>> list = new LinkedList<>(playsMap.entrySet());
         	Collections.sort(list, new Comparator<Map.Entry<Integer,Integer>>(){
 
-				@Override
-				public int compare(Entry<Integer, Integer> o1, Entry<Integer, Integer> o2) {
-					 int comparision = (o1.getValue() - o2.getValue()) * -1;
-		             return comparision == 0 ? o1.getKey().compareTo(o2.getKey()) : comparision;
-				}
-				
-            });
+        		@Override
+        		public int compare(Entry<Integer, Integer> o1, Entry<Integer, Integer> o2) {
+        			int comparision = (o1.getValue() - o2.getValue()) * -1;
+        			return comparision == 0 ? o1.getKey().compareTo(o2.getKey()) : comparision;
+        		}
+        	});
         	
+        	for(int j=0; j<2; j++) {
+        		answer[i] = list.get(j).getKey();
+        		i++;
+        	}
         }
-        
-        System.out.println(map);
-        
-
-    	
-        
-        
-       
-        
-        
         
         return answer;
     }
