e085887cdc4951dc6a4868eb83c0776361fba527
chucoding <chucoding@gmail.com>
2020-04-07 18:33:11 +0900

    프린터 문제 큐 두개 스택 하나로 풀었지만 나중에 스택 없애고 location줄여가면서 큐도 하나 빼보기

--- src/programmers/coding_test_high_score_kit/stack_queue/Printer.java
+++ src/programmers/coding_test_high_score_kit/stack_queue/Printer.java
@@ -1,23 +1,52 @@
 package programmers.coding_test_high_score_kit.stack_queue;
 
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Printer {
 
 	public static int solution(int[] priorities, int location) {
 		int answer = 0;
-		int max = 0;
-		int maxIndex = 0;
-		for(int i=0; i<priorities.length; i++) {
 
-			if(max < priorities[i]) {
-				max = priorities[i];
-				maxIndex = i;
+		Deque<Integer> stack = new ArrayDeque<Integer>();
+		Queue<Integer> valueQueue = new LinkedList<Integer>();
+		Queue<Integer> orderQueue = new LinkedList<Integer>();
+
+		for(int i=0; i<priorities.length; i++){
+			valueQueue.offer(priorities[i]);
+			orderQueue.offer(i);
+		}
+
+		Arrays.sort(priorities);
+
+		for(int i=0; i<priorities.length; i++){
+			stack.push(priorities[i]);
+		}
+
+		int priority = 0;
+		if(!stack.isEmpty()) priority = stack.pop();
+
+		while(!orderQueue.isEmpty() || !stack.isEmpty()){
+
+			int o = orderQueue.poll();
+			int v = valueQueue.poll();
+
+			if(v == priority) {
+
+				answer++;
+				if(!stack.isEmpty()) priority = stack.pop();
+				else break;
+
+				if(o == location) break;
+			} else{
+				orderQueue.offer(o);
+				valueQueue.offer(v);
 			}
 		}
-		
-		if(location >= maxIndex) answer = location - maxIndex + 1;
-		else answer = priorities.length - maxIndex + location + 1;
-		
+
 		return answer;
 	}
 	
