aad0400d3051d25927338a3b47a7bdb0b2d40b45
chucoding <chucoding@gmail.com>
2020-04-10 01:44:51 +0900

    디스크 컨트롤러 테스트케이스 통과

--- src/programmers/coding_test_high_score_kit/heap/DiskController.java
+++ src/programmers/coding_test_high_score_kit/heap/DiskController.java
@@ -1,8 +1,39 @@
 package programmers.coding_test_high_score_kit.heap;
 
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.PriorityQueue;
+
+class Disk implements Comparable<Disk> {
+
+	//comparable이 implements되지 않은 object를 sorting 하려고 하면 컴파일러가 comparable interface가 구현되어 있을거라고 예상하기 때문에 exception이 발생한다.
+	
+	private Integer requestTime;
+	private Integer processTime;
+	
+	public Disk(int requestTime, int processTime){
+		this.requestTime = requestTime;
+		this.processTime = processTime;
+	}
+	
+	public int getRequestTime() {
+		return requestTime;
+	}
+
+	public int getProcessTime() {
+		return processTime;
+	}
+
+	@Override
+	public int compareTo(Disk o) {
+		if(this.processTime - this.requestTime < o.processTime - o.requestTime) return -1;
+		else return 1;
+	}
+
+	@Override
+	public String toString() {
+		return "Disk [requestTime=" + requestTime + ", processTime=" + processTime + "]";
+	}
+}
 
 public class DiskController {
 
@@ -14,36 +45,34 @@ public class DiskController {
 			length += jobs[i][1];
 		}
 		
-		Arrays.sort(jobs, Comparator.comparingInt(o1 -> o1[0]));
-		PriorityQueue<Integer> heap = new PriorityQueue<Integer>();
+		Arrays.sort(jobs, (o1, o2) -> {
+			if(o1[0] == o2[0]) return Integer.compare(o1[1], o2[1]);
+			else return Integer.compare(o1[0], o2[0]);
+		});
+		
+		PriorityQueue<Disk> heap = new PriorityQueue<Disk>();
 		
 		int index = 0;
-		int n =0;
 		int endTime = 0;
+		Disk d;
 		
 		for(int i=0; i<length; i++) {
+			
 			while(index < jobs.length && i == jobs[index][0]) {
-				
-				if(!heap.isEmpty()) heap.offer( jobs[index][1] - jobs[index][0]);
-				else {
-					heap.offer(jobs[index][1]);
-					endTime = jobs[index][1];
-				}
+				heap.offer(new Disk(jobs[index][0], jobs[index][1]));
 				index++;
 			}
 			
-			System.out.println(heap);
-			
 			if(!heap.isEmpty() && endTime == 0) {
-				n = heap.poll();
-				endTime = i + n;
-				answer += endTime;
-				System.out.println(answer);
+				d = heap.poll();
+				answer += i + d.getProcessTime() - d.getRequestTime();
+				endTime += d.getProcessTime();
 			}
 			
 			endTime--;
 		}
-		
+
+		answer /= 3;
 		return answer;
 	}
 	
