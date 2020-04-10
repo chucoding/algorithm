a9ee02ece977a707a34df253bdcee2bccc4fb63e
chucoding <chucoding@gmail.com>
2020-04-10 13:08:01 +0900

    소수찾기 문제풀이 실패

--- src/programmers/coding_test_high_score_kit/bp/FindPrimeNumbers.java
+++ src/programmers/coding_test_high_score_kit/bp/FindPrimeNumbers.java
@@ -1,34 +1,40 @@
 package programmers.coding_test_high_score_kit.bp;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class FindPrimeNumbers {
 
-    public static int solution(String numbers) {
-        int answer = 0;
-        int[][] test = new int[numbers.length()][numbers.length()];
-        
-        for(int i=0; i<numbers.length(); i++) {
-        	for(int j=0; j<numbers.length(); j++) {
-        		test[i][j] = 1;
-        	}
-        }
-        
-        for(int i=0; i<numbers.length(); i++) {
-        	for(int j=0; j<numbers.length(); j++) {
-        		System.out.print(test[i][j]+" ");
-        	}
-        	System.out.println();
-        }
-        
-        return answer;
-    }
+	public static int solution(String numbers) {
+		int answer = 0;
+
+		char[] str = numbers.toCharArray();
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		
+		for(int i=0; i<str.length; i++) {
+			map.put(str[i]-48, map.getOrDefault(map.get(str[i]-48), 0)+1);
+		}
+
+		for(Integer i : map.keySet()) {
+			map.get(i);
+		}
+		
+//		Set<Integer> set = new HashSet<Integer>();
+//
+//		for(int i=0; i<numbers.length(); i++) {
+//			for(int j=i; j<numbers.length(); j++) {
+//				set.add(Integer.valueOf(numbers.substring(i, j+1)));
+//			}
+//		}
+//
+//		System.out.println(set);
+
+		return answer;
+	}
     
     public static void main(String[] args) {
 		
     	int a = solution("17");
-	
-    	
+    	int b = solution("011");
     }
 }
