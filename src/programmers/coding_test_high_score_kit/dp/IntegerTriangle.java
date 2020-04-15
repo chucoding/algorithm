7d6b8cff3191a0ffe4c433fed129ff1ac31df528
chucoding <chucoding@gmail.com>
2020-04-15 17:29:26 +0900

    정수 삼각형 static 변수 제거

--- src/programmers/coding_test_high_score_kit/dp/IntegerTriangle.java
+++ src/programmers/coding_test_high_score_kit/dp/IntegerTriangle.java
@@ -2,37 +2,30 @@ package programmers.coding_test_high_score_kit.dp;
 
 public class IntegerTriangle {
 
-	private static int[][] triangles = null;
-	
 	public static int solution(int[][] triangle) {
-	
-		int answer = 0;
-		triangles = triangle;
-		
-		answer = recursion(triangle.length-1, 0);
-		System.out.println(answer);
-		
-		return answer;
-	}
-	
-	public static int recursion(int i, int j) {
 
-		//탈출조건
-		if(i == 0) return triangles[i][j];
-		
-		if(triangles[i][j] > triangles[i][j+1]) triangles[i-1][j] += triangles[i][j];
-		else triangles[i-1][j] += triangles[i][j+1];
-		
-		j+=1;
-		
-		if(j+1 > triangles[i].length-1) {
-			i -= 1;
-			j = 0;
-		}
-		
-		return recursion(i, j);
-		
-	}
+        int answer = 0;
+        answer = recursion(triangle.length-1, 0, triangle);
+        return answer;
+    }
+
+    public static int recursion(int i, int j, int[][] triangle) {
+
+        if(i == 0) return triangle[i][j];
+
+        if(triangle[i][j] > triangle[i][j+1]) triangle[i-1][j] += triangle[i][j];
+        else triangle[i-1][j] += triangle[i][j+1];
+
+        j+=1;
+
+        if(j > triangle[i].length-2) {
+            i -= 1;
+            j = 0;
+        }
+
+        return recursion(i, j, triangle);
+
+    }
 
 	public static void main(String[] args) {
 		int answer = solution(new int[][] {{7},{3,8},{8,1,0},{2,7,4,4},{4,5,2,6,5}});
