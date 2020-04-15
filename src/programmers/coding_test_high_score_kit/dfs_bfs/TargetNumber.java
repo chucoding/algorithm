41488720f2234a07c0fa34ad256dc259723b1e46
chucoding <chucoding@gmail.com>
2020-04-15 16:27:52 +0900

    타겟 넘버 문제풀이 완료

--- src/programmers/coding_test_high_score_kit/dfs_bfs/TargetNumber.java
+++ src/programmers/coding_test_high_score_kit/dfs_bfs/TargetNumber.java
@@ -2,27 +2,32 @@ package programmers.coding_test_high_score_kit.dfs_bfs;
 
 public class TargetNumber {
 
+	private static int count = 0;
+
+	public static void dfs(int[] arr, int i, int sum, int target){
+
+		if(i == arr.length) {
+			if(sum == target) count++;
+			return;
+		}
+
+		dfs(arr, i+1, sum+arr[i], target);
+		dfs(arr, i+1, sum-arr[i], target);
+
+	}
+
 	public static int solution(int[] numbers, int target) {
-        
 		int answer = 0;
-        answer = dfs(numbers, 0, 0, target);
-        return answer;
-    }
-	
+
+		dfs(numbers, 0, 0, target);
+		answer = count;
+
+		return answer;
+	}
+
 	public static void main(String[] args) {
 		System.out.println(solution(new int[]{1,1,1,1,1}, 3));
 		System.out.println(solution(new int[]{1,1,1,1,1,1}, 2));
-	}
-	
-	public static int dfs(int[] numbers, int node, int sum, int target) {
-		if(node == numbers.length) {
-			if(sum == target)
-				return 1;
-			return 0;
-		}
-		
-		return dfs(numbers, node+1, sum+numbers[node], target) + dfs(numbers, node+1, sum - numbers[node], target);
-		
 	}
 		
 }
