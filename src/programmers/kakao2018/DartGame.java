0347289f347ffdb9931f6364dbcc4e309b671055
chucoding <chucoding@gmail.com>
2020-04-17 14:49:39 +0900

    다트게임 풀이

--- src/programmers/kakao2018/DartGame.java
+++ src/programmers/kakao2018/DartGame.java
@@ -8,21 +8,31 @@ public class DartGame {
 		char[] str = dartResult.toCharArray();
 		int[] score = new int[3];
 		
+		StringBuilder sb = new StringBuilder();
+		
 		int index=0;
 		for(int i=0; i<str.length; i++) {
-			if(str[i] >= 48 && str[i] <= 57) score[index++] = str[i]-48;
-			else if(str[i] == 'D') score[index-1] = (int) Math.pow(score[index - 1], 2);
-			else if(str[i] == 'T') score[index-1] = (int) Math.pow(score[index - 1], 3);
-			else if(str[i] == '*') {
-				score[index-1] *=2;
-				if(index >= 2) score[index-2] *= 2;
-			} else if(str[i] == '#') {
-				score[index-1] *= -1;
+			
+			if(str[i] >= 48 && str[i] <= 57) sb.append(str[i] - 48);
+			else {
+				if(sb.length()!=0) {
+					score[index++] = Integer.parseInt(new String(sb));
+					sb.setLength(0);
+				}
+				
+				if(str[i] == 'D') score[index-1] = (int) Math.pow(score[index - 1], 2);
+				if(str[i] == 'T') score[index-1] = (int) Math.pow(score[index - 1], 3);
+				if(str[i] == '*') {
+					score[index-1] *=2;
+					if(index >= 2) score[index-2] *= 2;
+				} else if(str[i] == '#') {
+					score[index-1] *= -1;
+				}
 			}
 		}
 		
 		for(int i=0; i<score.length; i++) {
-			System.out.println(score[i]);
+			answer+=score[i];
 		}
 		
 		return answer;
