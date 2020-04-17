8300f5e3ee6d2bd790724cb8fe597a837d3d793f
chucoding <chucoding@gmail.com>
2020-04-17 17:04:07 +0900

    위장 다시 풀기

--- src/programmers/coding_test_high_score_kit/hash/Camouflage.java
+++ src/programmers/coding_test_high_score_kit/hash/Camouflage.java
@@ -5,24 +5,20 @@ import java.util.Map;
 
 public class Camouflage {
 
-	 public static int solution(String[][] clothes) {
-	     int answer = 1;
-	     
-	     Map<String, Integer> map = new HashMap<String, Integer>();
-	     
-	     for(int i=0; i<clothes.length; i++) {
-	    	map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0)+1);
-	     }
-	     
-	     if(map.size() == 1) answer = clothes.length;
-	     else {
-	    	 for ( String key : map.keySet() ) {
-		    	 answer *= map.get(key)+1;
-		     }
-	    	 answer -=1;
-	     }
-	     
-	     return answer;
+	public static int solution(String[][] clothes) {
+		int answer = 1;
+
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		for(int i=0; i<clothes.length; i++){
+			map.put(clothes[i][1], map.getOrDefault( clothes[i][1], 0) +1); 
+		}
+
+		for(String s : map.keySet()){
+			answer *= map.get(s) + 1;
+		}
+		answer -= 1;
+
+		return answer;
 	 }
 	 
 	 public static void main(String[] args) {
