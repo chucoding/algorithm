6dd661bd8730ea77897ae0c0408fcb14eabf7010
chucoding <chucoding@gmail.com>
2020-04-17 22:59:29 +0900

    큰 수 만들기 풀이 수정

--- src/programmers/coding_test_high_score_kit/greedy/CreateLargeNumber.java
+++ src/programmers/coding_test_high_score_kit/greedy/CreateLargeNumber.java
@@ -1,37 +1,48 @@
 package programmers.coding_test_high_score_kit.greedy;
 
-import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class CreateLargeNumber {
 
     public static String solution(String number, int k) {
-        char[] result = new char[number.length() - k];
-        Stack<Character> stack = new Stack<>();
-
-        for (int i=0; i<number.length(); i++) {
-            char c = number.charAt(i);
-            while (!stack.isEmpty() && stack.peek() < c && k-- > 0) {
-                stack.pop();
-            }
-            stack.push(c);
-        }
-        for (int i=0; i<result.length; i++) {
-            result[i] = stack.get(i);
-        }
-        
-        System.out.println(new String(result));
-        return new String(result);
+    	String answer = "";
+    	
+    	Deque<Integer> st = new ArrayDeque<Integer>();
+    	for(int i=0; i<number.length(); i++) {
+    		int n = Character.getNumericValue(number.charAt(i));
+    		while(!st.isEmpty()) {
+    			if(st.peek() >= n || k == 0) break;
+    			else {
+    				st.pop();
+    				k--;
+    			}
+    		}
+    		
+    		st.push(n);
+    	}
+    	
+    	StringBuilder sb = new StringBuilder();
+    	while(!st.isEmpty()) {
+    		sb.append(st.pop());
+    	}
+    	
+    	answer = new String(sb.reverse());
+    	if(answer.length() > number.length() - k) answer = answer.substring(0,number.length() - k);
+    	System.out.println(answer);
+        return answer;
     }
     
 	public static void main(String[] args) {
 
-		solution("19241", 3);
-		solution("1231234", 4);
-		solution("4177252841",5);
-		solution("410002841",4);
-		solution("000002841",4);
+//		solution("19241", 3);
+//		solution("1231234", 4);
+//		solution("4177252841",5);
+//		solution("410002841",4);
+//		solution("000002841",4);
 		solution("7032841",5);
-		solution("0000000",6);
+		solution("4177252841",4);
+		solution("7777777",3);
 	}
 }
 
