97b7b15bccfc16ded1c44015b89d595bb717739e
chucoding <chucoding@gmail.com>
2020-04-18 22:14:26 +0900

    주식가격 배열풀이(속도 더 빠름)

--- src/programmers/coding_test_high_score_kit/stack_queue/StockPrice2.java
+++ src/programmers/coding_test_high_score_kit/stack_queue/StockPrice2.java
@@ -5,23 +5,16 @@ public class StockPrice2 {
 	public static int[] solution(int[] prices) {
 
 		int[] answer = new int[prices.length];
-        int count = 0;
         
         for(int i=0; i<prices.length-1; i++) {
-            count = 0;
             for(int j=i+1; j<prices.length; j++) {
-                if(prices[i]<=prices[j]) {
-                    count++;
-                } else {
-                    count++;
-                    break;
+                if(prices[i] > prices[j]) {
+                	answer[i] = j-i;
+                	break;
                 }
+                if(j==answer.length-1) answer[i] = j-i;
             }
-            answer[i] = count;
         }
-        
-        answer[prices.length-1] = 0;
-        
         return answer;
     }
 
