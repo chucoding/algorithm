de8ffc2bc211fdc4b820fb25e91fbc95a5081bd4
chucoding <chucoding@gmail.com>
2020-04-19 16:03:24 +0900

    프린터 리팩토링

--- src/programmers/coding_test_high_score_kit/stack_queue/Printer.java
+++ src/programmers/coding_test_high_score_kit/stack_queue/Printer.java
@@ -1,54 +1,41 @@
 package programmers.coding_test_high_score_kit.stack_queue;
 
-import java.util.ArrayDeque;
 import java.util.Arrays;
-import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class Printer {
 
 	public static int solution(int[] priorities, int location) {
-		int answer = 0;
-
-		Deque<Integer> stack = new ArrayDeque<Integer>();
-		Queue<Integer> valueQueue = new LinkedList<Integer>();
-		Queue<Integer> orderQueue = new LinkedList<Integer>();
-
-		for(int i=0; i<priorities.length; i++){
-			valueQueue.offer(priorities[i]);
-			orderQueue.offer(i);
-		}
-
-		Arrays.sort(priorities);
-
-		for(int i=0; i<priorities.length; i++){
-			stack.push(priorities[i]);
-		}
-
-		int priority = 0;
-		if(!stack.isEmpty()) priority = stack.pop();
-
-		while(!orderQueue.isEmpty() || !stack.isEmpty()){
-
-			int o = orderQueue.poll();
-			int v = valueQueue.poll();
-
-			if(v == priority) {
-
-				answer++;
-				if(!stack.isEmpty()) priority = stack.pop();
-				else break;
-
-				if(o == location) break;
-			} else{
-				orderQueue.offer(o);
-				valueQueue.offer(v);
-			}
-		}
-
-		return answer;
-	}
+        int answer = 0;
+        
+        Queue<Integer> indexQ = new LinkedList<Integer>();
+        Queue<Integer> valueQ = new LinkedList<Integer>();
+        
+        for(int i=0; i<priorities.length; i++){
+            indexQ.offer(i);
+            valueQ.offer(priorities[i]);
+        }
+        
+        Arrays.sort(priorities);
+        
+        int i = priorities.length - 1;
+        while(!indexQ.isEmpty()){
+            
+            if(priorities[i] == valueQ.peek()){
+                valueQ.poll();
+                if(location == indexQ.poll()) break;
+                 i--;
+                 answer++;
+                
+            } else {
+                indexQ.offer(indexQ.poll());
+                valueQ.offer(valueQ.poll());
+            }
+        }
+        
+        return answer+1;
+    }
 	
 	public static void main(String[] args) {
 		System.out.println(solution(new int[] {1,6,3,1,2,5,6,7,2,3}, 3));
