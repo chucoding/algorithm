96e305f3be30632488d49d0f50a9c75f54e8803c
chucoding <chucoding@gmail.com>
2020-04-22 10:15:27 +0900

    자릿수 더하기 수행속도 비교

--- src/programmers/codingtest_practice/adding_digit/AddingDigits.java
+++ src/programmers/codingtest_practice/adding_digit/AddingDigits.java
@@ -1,14 +1,19 @@
-package programmers.codingtest_practice;
+package programmers.codingtest_practice.adding_digit;
 
 public class AddingDigits {
 
 	public static int solution(int n) {
 		int answer = 0;
 		
+		long a = System.nanoTime();
+		
 		String s = String.valueOf(n);
 		for(int i=0; i<s.length(); i++) {
 			answer += Character.getNumericValue(s.charAt(i));
 		}
+		
+		long b = System.nanoTime();
+        System.out.println(b-a);
 		
 		return answer;
 	}
