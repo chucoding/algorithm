aeec48f620deeac395186414f3eecfc4e2cd976f
chucoding <chucoding@gmail.com>
2020-04-23 16:04:47 +0900

    124나라의 숫자 문제풀이

---
+++ src/programmers/codingtest_practice/numberOf124Countries/NumberOf124Countries2.java
@@ -0,0 +1,14 @@
+package programmers.codingtest_practice.numberOf124Countries;
+
+public class NumberOf124Countries2 {
+
+	public static String solution(int n) {
+		String answer = "";
+	     
+	      return answer;
+	}
+	
+	public static void main(String[] args) {
+		solution(2);
+	}
+}
