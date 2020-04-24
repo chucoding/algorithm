ab5de74e010b829d6e414568b24b54e204ab907e
chucoding <chucoding@gmail.com>
2020-04-24 13:12:21 +0900

    스킬트리 - 큐로 접근했으나 중간삭제를 못하는 문제발생

--- src/programmers/summer_winter_2018/skill_tree/SkillTree.java
+++ src/programmers/summer_winter_2018/skill_tree/SkillTree.java
@@ -1,25 +1,46 @@
 package programmers.summer_winter_2018.skill_tree;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class SkillTree {
 
 	public static int solution(String skill, String[] skill_trees) {
         int answer = 0;
         
         char[] s = skill.toCharArray();
-
+        Deque<Character> q = new ArrayDeque<Character>();
+        
+        int t = 0;
         for(int i=0; i<skill_trees.length; i++) {
 
-        	int k=0;
+        	for(int j=t; j<s.length; j++) {
+            	q.offer(s[j]);
+            }
+        	
+        	boolean b = true;
+        	
         	for(int j=0; j<skill_trees[i].length(); j++) {
-        		
-        		if(skill_trees[i].charAt(j) == s[k]) k++;
-        		if(k == skill.length()-1) {
-        			answer++;
-        			break;
+
+        		if(!q.isEmpty() && skill_trees[i].charAt(j) == q.peek()) q.poll();
+        		else {
+        			while(!q.isEmpty()) {
+        				if(skill_trees[i].charAt(j) == q.poll()) {
+        					b = false;
+        					t++; 
+        					//중간삭제?? list필요
+        					break;
+        				}
+        			}
         		}
+        		if(!b) break;
         	}
+        	
+        	q.clear();
+        	if(b) answer++;
         }
         
+        System.out.println(answer);
         return answer;
     }
 	
