d40a7c564e9b3b5ecf8410fa52041e3934e7a81d
chucoding <chucoding@gmail.com>
2020-04-25 11:23:07 +0900

    스킬토리 리팩토링 및 수행속도 측정

--- src/programmers/summer_winter_2018/skill_tree/SkillTree3.java
+++ src/programmers/summer_winter_2018/skill_tree/SkillTree3.java
@@ -1,18 +1,12 @@
 package programmers.summer_winter_2018.skill_tree;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-
 public class SkillTree3 {
 
 	public static int solution(String skill, String[] skill_trees) {
-        int answer = 0;
+        int answer = skill_trees.length;
 
         long a = System.nanoTime();
         
-        ArrayList list = new ArrayList(Arrays.asList(skill.toCharArray()));
-        answer = skill_trees.length;
         for(int i=0; i<skill_trees.length; i++){
             if(skill.indexOf(skill_trees[i].replaceAll("[^"+skill+"]",""))!=0) {
                 answer--;
