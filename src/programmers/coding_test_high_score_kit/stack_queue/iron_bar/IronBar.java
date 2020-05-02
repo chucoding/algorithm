d6cf62d4761c5473dbf3c5b8590798b9dc521d5b
chucoding <chucoding@gmail.com>
2020-05-02 13:56:29 +0900

    프로그래머스 쇠막대기 문제 쇠막대기 3번째 시작부분 문제됨

---
+++ src/programmers/coding_test_high_score_kit/stack_queue/iron_bar/IronBar.java
@@ -0,0 +1,41 @@
+package programmers.coding_test_high_score_kit.stack_queue.iron_bar;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class IronBar {
+	
+	public static int solution(String arrangement) {
+        int answer = 0;
+        
+        Deque<Character> stack = new ArrayDeque<Character>();
+        char[] c = arrangement.toCharArray();
+        
+        boolean b = true;
+        for(int i=0; i<c.length; i++) {
+        	
+        	if(c[i] == '(') {
+        		stack.push(c[i]);
+        		b = true;
+        	}
+        	else {
+        		
+        		if(!b) {
+        			stack.pop();
+        			answer += stack.size() + 1;
+        		}else {
+        			stack.pop();
+        			answer += stack.size();
+        			b = false;
+        		}
+        		System.out.println(answer);
+        	}
+        }
+        
+        return answer;
+    }
+	
+	public static void main(String[] args) {
+		solution("()(((()())(())()))(())");
+	}
+}
