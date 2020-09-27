7a528d91d1c1883bb0b96c65fa51e06059bc780a
chucoding <chucoding@gmail.com>
2020-09-27 23:37:29 +0900

    [Baekjoon] n과 M 문제풀이중

--- src/baekjoon/backtracking/No15665_NnM.java
+++ src/baekjoon/backtracking/No15665_NnM.java
@@ -3,6 +3,9 @@ package baekjoon.backtracking;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class No15665_NnM {
@@ -15,10 +18,36 @@ public class No15665_NnM {
 		int cnt = Integer.parseInt(st.nextToken());
 		
 		st = new StringTokenizer(br.readLine());
-		String[] arr = new String[len];
 		
+		int[] arr = new int[len];
 		for(int i=0; i<len; i++) {
-			arr[i] = st.nextToken();
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		
+		List<Integer> nList = new ArrayList<Integer>();
+		
+		nList.add(arr[0]);
+		for(int i=1; i<len; i++) {
+			if(arr[i] != arr[i-1]) {
+				nList.add(arr[i]);
+			}
+		}
+
+		for(int i=0; i<nList.size(); i++) {
+			for(int j=0; j<cnt; j++) {
+				getSequence(nList.get(i), i, 0, cnt);
+			}
+		}
+		
+//		System.out.print(nList.get(i)+" ");
+		
+	}
+	
+	public static void getSequence(int n, int length, int j, int cnt) {
+		for(int k=0; k<cnt; k++) {
+			
 		}
 	}
 }
