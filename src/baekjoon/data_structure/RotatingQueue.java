fa1ab91821b9f2358e84432455ffac8f77cba1dc
chucoding <chucoding@gmail.com>
2020-10-18 22:07:21 +0900

    [baekjoon]회전하는큐 문제풀이중

--- src/baekjoon/data_structure/RotatingQueue.java
+++ src/baekjoon/data_structure/RotatingQueue.java
@@ -20,18 +20,75 @@ public class RotatingQueue {
 		int n = Integer.parseInt(st.nextToken());
 		
 		Deque<Integer> queue = new ArrayDeque<Integer>();
+		
 		for(int i=0; i<size; i++) {
 			queue.offer(i+1);
 		}
 		
 		st = new StringTokenizer(br.readLine());
-		int[] order = new int[n];
+		int[] position = new int[n];
 		for(int i=0; i<n; i++) {
-			order[i] = Integer.parseInt(st.nextToken());
+			position[i] = Integer.parseInt(st.nextToken());
 		}
 		
-//		queue.pollFirst();
-//		queue.pollLast();
+		int count = 0;
+		int dump = 0;
+		boolean b = true;
+
+		for(int i=0; i<position.length; i++) {
+
+			int qsize = queue.size();
+			int pos = position[i] - 1;
+			if(b) pos += dump;
+			else pos -= dump;
+			dump = 0;
+			int item = -1;
+
+			while (true) {
+
+				if (queue.isEmpty()) break;
+
+				if (qsize - pos > pos - 1) {
+					item = queue.pollFirst();
+					if (position[i] != item) {
+						b = false;
+						queue.addLast(item);
+						count++;
+						dump++;
+					} else break;
+				} else if(qsize - pos < pos - 1) {
+					item = queue.pollLast();
+					count++;
+					b = true;
+					dump++;
+					if (position[i] != item) {
+						queue.addFirst(item);
+					} else break;
+				} else if(((qsize - pos) == (pos - 1)) && i<position.length-1 ) {
+					int pos2 = position[i+1];
+					if(qsize - pos2 > pos2 - 1) {
+						item = queue.pollFirst();
+						if (position[i] != item) {
+							queue.addLast(item);
+							count++;
+							dump++;
+							b = false;
+						} else break;
+					} else {
+						item = queue.pollLast();
+						count++;
+						dump++;
+						b=true;
+						if (position[i] != item) {
+							queue.addFirst(item);
+						} else break;
+					}
+				}
+			}
+			System.out.println(count);
+			System.out.println(queue);
+		}
 		
+		System.out.println(count);
 	}
 }
