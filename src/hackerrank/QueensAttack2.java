dfaf4234b52134dbb6c91e6685570aec6f301d01
chucoding <chucoding@gmail.com>
2020-12-08 01:03:59 +0900

    [HackerRank] Queen's Attack2 문제풀이

--- src/hackerrank/QueensAttack2.java
+++ src/hackerrank/QueensAttack2.java
@@ -8,103 +8,95 @@ public class QueensAttack2 {
     // Complete the queensAttack function below.
     static int queensAttack(int n, int k, int r_q, int c_q, int[][] obstacles) {
 
-    	int count = 0;
-    	int[][] board = new int[n][n];
+    	int sum = 0;
+    	int[] count = new int[8];
+    	
+    	for(int i=0; i<8; i++) {
+    		count[i] = n;
+    	}
     	
     	for(int i=0; i<k; i++) {
-    		board[obstacles[i][0]-1][obstacles[i][1]-1] = 1;
-    	}
-    	
-		int x = r_q-1;
-		int y = c_q-1;
-    	
-    	//왼쪽 위 대각선
-    	while(true) {
+
+    		int y = obstacles[i][0];
+    		int x = obstacles[i][1];
     		
-    		x++;
-    		y--;
-    		if(x > n-1 || y < 0 || board[x][y] == 1) break;
-    		count++;
+    		if(Math.abs(y - r_q) == Math.abs(x - c_q) || x == c_q || y == r_q) {
+    			if(r_q < y && c_q > x) {
+        			int min = Math.min(y-r_q, c_q-x);
+        			if(count[0] > min) {
+        				count[0] = min-1;
+        			}
+        		} else if(r_q < y && c_q == x) {
+        			int min = y - r_q;
+        			if(count[1] > min) {
+        				count[1] = min-1;
+        			}
+        		} else if(r_q < y && c_q < x) {
+        			int min = Math.min(y-r_q, x-c_q);
+        			if(count[2] > min) {
+        				count[2] = min-1;
+        			}
+        		} else if(r_q == y && c_q < x) {
+        			int min = x - c_q;
+        			if(count[3] > min) {
+        				count[3] = min-1;
+        			}
+        		} else if(r_q > y && c_q < x) {
+        			int min = Math.min(r_q-y, x-c_q);
+        			if(count[4] > min) {
+        				count[4] = min-1;
+        			}
+        		} else if(r_q > y && c_q == x) {
+        			int min = r_q - y;
+        			if(count[5] > min) {
+        				count[5] = min-1;
+        			}
+        		} else if(r_q > y && c_q > x) {
+        			int min = Math.min(r_q-y, c_q-x);
+        			if(count[6] > min) {
+        				count[6] = min-1;
+        			}
+        		} else if(r_q == y && c_q > x) {
+        			int min = c_q - x;
+        			if(count[7] > min) {
+        				count[7] = min-1;
+        			}
+        		}
+    		}
+    	}
+    	for(int i=0; i<8; i++) {
+    		if(count[i] == n) {
+    			switch(i) {
+    			case 0 :
+    				sum += Math.min(n - r_q, c_q - 1);
+    				break;
+    			case 1 :
+    				sum += n - r_q;
+    				break;
+    			case 2 :
+    				sum += Math.min(n - r_q, n - c_q);
+    				break;
+    			case 3 :
+    				sum += n - c_q;
+    				break;
+    			case 4 : 
+    				sum += Math.min(r_q - 1, n - c_q);
+    				break;
+    			case 5 :
+    				sum += r_q - 1;
+    				break;
+    			case 6 :
+    				sum += Math.min(r_q - 1, c_q - 1);
+    				break;
+    			case 7 :
+    				sum += c_q - 1;
+    				break;
+    			}
+    		}
+    		else sum += count[i];
     	}
     	
-    	x = r_q-1;
-    	y = c_q-1;
-    	
-    	//위
-    	while(true) {
-    		
-    		x++;
-    		if(x > n-1 || board[x][y] == 1) break;
-    		count++;
-    	}
-    	
-    	x = r_q-1;
-    	y = c_q-1;
-    	
-    	//오른쪽 위 대각선
-    	while(true) {
-    		
-    		x++;
-    		y++;
-    		if(x > n-1 || y > n-1 || board[x][y] == 1) break;
-    		count++;
-    	}
-    	
-    	x = r_q-1;
-    	y = c_q-1;
-    	
-    	//오른쪽
-    	while(true) {
-    		
-    		y++;
-    		if(y > n-1 || board[x][y] == 1) break;
-    		count++;
-    	}
-    	
-    	x = r_q-1;
-    	y = c_q-1;
-    	
-    	//오른쪽 아래 대각선
-    	while(true) {
-    		
-    		x--;
-    		y++;
-    		if(x < 0 || y > n-1 || board[x][y] == 1) break;
-    		count++;
-    	}
-    	
-    	x = r_q-1;
-    	y = c_q-1;
-    	
-    	//아래
-    	while(true) {
-    		x--;
-    		if(x < 0 || board[x][y] == 1) break;
-    		count++;
-    	}
-    	
-    	x = r_q-1;
-    	y = c_q-1;
-    	
-    	//왼쪽 아래 대각선
-    	while(true) {
-    		x--;
-    		y--;
-    		if(x < 0 || y < 0 || board[x][y] == 1) break;
-    		count++;
-    	}
-    	
-    	x = r_q-1;
-    	y = c_q-1;
-    	
-    	//왼
-    	while(true) {
-    		y--;
-    		if(y < 0 || board[x][y] == 1) break;
-    		count++;
-    	}
-    	
-    	return count;
+    	return sum;
     }
     
     private static final Scanner scanner = new Scanner(System.in);
