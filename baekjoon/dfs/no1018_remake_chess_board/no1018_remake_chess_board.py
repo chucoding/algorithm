ea5818004966cee45f0b8971e47515f52071ec03
chucoding <chucoding@gmail.com>
2022-05-01 17:26:47 +0900

    체스판 다시 칠하기(접근방법 틀림 다시 확인필요)

--- baekjoon/dfs/no1018_remake_chess_board/no1018_remake_chess_board.py
+++ baekjoon/dfs/no1018_remake_chess_board/no1018_remake_chess_board.py
@@ -1,13 +1,13 @@
-global visited
-global cnt
-
-def recursive(a, b, i, j, board) :
-    if i < 0 or j < 0 or i > len(board)-1 or j > len(board[i])-1 or board[i][j] == board[a][b]:
+def recursive(a, b, i, j) :
+    if i < 0 or j < 0 or i > len(board)-1 or j > len(board[i])-1 or ((i!=a or j!=b) and board[i][j] == board[a][b]) or visited[i][j] == 1 :
         return
     visited[i][j] = 1
+    global cnt
     cnt+=1
-    recursive(i, j, i, j+1, board)
-    recursive(i, j, i+1, j, board)
+    recursive(i, j, i, j-1)
+    recursive(i, j, i-1, j)
+    recursive(i, j, i, j+1)
+    recursive(i, j, i+1, j)
 
 num = input().split(" ")
 board = []
@@ -18,9 +18,11 @@ for i in range(int(num[0])) :
         l.append(s)
     board.append(l)
 
+result = []
 for i, y in enumerate(visited) :
     for j, x in enumerate(visited[i]) :
         if x == 0 :
             cnt = 0
-            recursive(i, j, i, j+1, board)
-            print(cnt)
+            recursive(i, j, i, j)
+            result.append(cnt)
+print(result)
