4ca14e5bea57e8867e080c7eb083409b2b9e77c2
chucoding <chucoding@gmail.com>
2023-07-03 23:48:58 +0900

    💯 소수 찾기

--- programmers/코딩 기초 트레이닝/find_prime_num/find_prime_num.py
+++
@@ -1,9 +0,0 @@
-def solution(n):
-    l = [0 for i in range(n+1)]
-    for i in range(2, n+1) :
-        for j in range(2, int(n**1/2)+1) :
-            if(i*j > n): break
-            l[i*j] = 1
-
-    s = [i for i in l[2:n+1] if i == 0]
-    return len(s)
